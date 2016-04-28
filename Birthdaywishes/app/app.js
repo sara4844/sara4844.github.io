e90222e985e0d94717b3996159e38f17ac513cf2
sara4844 <sraleroy@gmail.com>
2016-04-28 11:21:02 -0400

    added front page button to create birthday wish list

--- Birthdaywishes/app/app.js
+++ Birthdaywishes/app/app.js
@@ -1,12 +1,18 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var app = angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
+]);
+app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+app.controller('myCtrl', ['$scope', function($scope) {
+  $scope.count = 0;
+  $scope.myFunction = function() {
+    $scope.count++;
+  }
 }]);
